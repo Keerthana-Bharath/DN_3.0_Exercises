3c5f734684247b0c9d5be3f89f520e23d0284f2c
Keerthana-Bharath <113038156+Keerthana-Bharath@users.noreply.github.com>
2024-07-31 11:14:27 +0530

    Create strategy_pattern.java

---
+++ Week-1/Design Principles/Ex-8/strategy_pattern.java
@@ -0,0 +1,64 @@
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+    private String cardHolderName;
+    private String expiryDate;
+
+    public CreditCardPayment(String cardNumber, String cardHolderName, String expiryDate) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing credit card payment of $" + amount);
+        System.out.println("Card Number: " + cardNumber);
+        System.out.println("Card Holder: " + cardHolderName);
+        System.out.println("Expiry Date: " + expiryDate);
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount);
+        System.out.println("PayPal Email: " + email);
+    }
+}
+
+class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public PaymentContext(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
+
+public class StrategyPatternExample {
+    public static void main(String[] args) {
+        PaymentStrategy creditCardPayment = new CreditCardPayment("1234-5678-9876-5432", "John Doe", "12/25");
+        PaymentStrategy payPalPayment = new PayPalPayment("john.doe@example.com");
+
+        PaymentContext paymentContext = new PaymentContext(creditCardPayment);
+        System.out.println("Making payment with Credit Card:");
+        paymentContext.executePayment(100.0);
+
+        paymentContext = new PaymentContext(payPalPayment);
+        System.out.println("Making payment with PayPal:");
+        paymentContext.executePayment(200.0);
+    }
+}
