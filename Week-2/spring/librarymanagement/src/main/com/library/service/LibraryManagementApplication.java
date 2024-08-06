6554c0d2d067446967d26a66b403edf4fd3d7a26
Keerthana-Bharath <113038156+Keerthana-Bharath@users.noreply.github.com>
2024-08-06 19:45:47 +0530

    Update LibraryManagementApplication.java

--- Week-2/spring/librarymanagement/src/main/com/library/service/LibraryManagementApplication.java
+++ Week-2/spring/librarymanagement/src/main/com/library/service/LibraryManagementApplication.java
@@ -6,10 +6,10 @@ import com.library.service.BookService;
 
 public class LibraryManagementApplication {
     public static void main(String[] args) {
-       
+
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        BookService bookService = (BookService) context.getBean("bookService");
+        BookService bookService = context.getBean(BookService.class);
 
         bookService.manageBooks();
     }
