1ce7197d4a75f6754922cf2fe19e3fa8f4f3075d
Keerthana-Bharath <113038156+Keerthana-Bharath@users.noreply.github.com>
2024-08-06 19:52:22 +0530

    Update LoggingAspect.java

--- Week-2/spring/librarymanagement/src/main/java/com/library/aspect/LoggingAspect.java
+++ Week-2/spring/librarymanagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -1,25 +1,23 @@
 package com.library.aspect;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
 
-    @Pointcut("execution(* com.library.service.*.*(..))")
-    public void serviceMethods() {}
+    @Before("execution(* com.library.service.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Before method: " + joinPoint.getSignature().getName());
+    }
 
-    @Around("serviceMethods()")
-    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-        long start = System.currentTimeMillis();
-
-        Object proceed = joinPoint.proceed();
-
-        long executionTime = System.currentTimeMillis() - start;
-        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
-
-        return proceed;
+    @After("execution(* com.library.service.*.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        System.out.println("After method: " + joinPoint.getSignature().getName());
     }
 }
+
